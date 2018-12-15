7b4a4646f4d2062f8031c5e99094aacc311dbc15
Oláh Tamás <tolah94@gmail.com>
2018-12-15 18:03:00 +0100

    Added CalculatorService interface

---
+++ src/com/codecool/calculator/service/CalculatorService.java
@@ -0,0 +1,13 @@
+package com.codecool.calculator.service;
+
+public interface CalculatorService {
+	
+	public Float add(float a, float b);
+	
+	public Float subtract(float a, float b);
+	
+	public Float multiply(float a, float b);
+	
+	public Float division(float a, float b);
+	
+}
